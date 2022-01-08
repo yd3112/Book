2147bebe23bb7b8362d3692884c7a300b0f1374b
yd <pokar.yashusmile@gmail.com>
2022-01-09 00:56:04 +0530

    initialcommit

---
+++ blog/views.py
@@ -0,0 +1,23 @@
+from .models import Post
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView
+
+# Create your views here.
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body','pageno', ]
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body','pageno',]
